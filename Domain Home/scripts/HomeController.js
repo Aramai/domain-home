4f17aac3f37f3cabb163237c77ac87730376647d
Sean Clark <sean@sean-clark.com>
2015-10-03 18:53:30 -0400

    Inital check-in

---
+++ Domain Home/scripts/HomeController.js
@@ -0,0 +1,27 @@
+﻿(function () {
+
+    var app = angular.module("home", []);
+
+    var HomeController = function ($scope, $http, $log) {      
+
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url, { params: { username: "Aramai" } })
+              .then(onRepos, onError);
+        };
+
+        var onRepos = function (response) {
+            $scope.repos = response.data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the user";
+        };
+        
+        $http.get("https://api.github.com/users/Aramai")
+                .then(onUserComplete, onError);
+    };
+
+    app.controller("HomeController", HomeController);
+
+}());
